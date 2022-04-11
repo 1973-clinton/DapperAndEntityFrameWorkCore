558c7fa2bc73fcd7de5d238801cc72782d98c37a
1973-clinton <clintonboamahofficial@gmail.com>
2022-04-11 21:13:23 +0000

    Add project files.

---
+++ DapperAndEntityFrameWorkCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DapperAndEntityFrameWorkCourse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperAndEntityFrameWorkCourse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; }
+    }
+}
